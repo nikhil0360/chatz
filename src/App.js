5e7f57834a20a855fcf2c81fa64658e9e404e57a
Nikhil Agarwal <nikhil.agarwal@iiitb.org>
2021-07-17 18:46:46 +0530

    chat UI, functions redux, imported firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from './features/Counter';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
@@ -7,12 +6,11 @@ import Chat from './Chat';
 function App() {
   return (
     <div className="app">
-     
-      {/* Sidebar */}
-      <Sidebar />
-      <Chat />
-      {/* Chat */}
 
+      <Sidebar />
+
+      <Chat />
+      
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCQbQiofr4KKpv-g5Ey8qYpe0PpnaH9UjE",
+    authDomain: "chatz-1268c.firebaseapp.com",
+    projectId: "chatz-1268c",
+    storageBucket: "chatz-1268c.appspot.com",
+    messagingSenderId: "644231568871",
+    appId: "1:644231568871:web:194edcab168b1f76e4b142",
+    measurementId: "G-1K5KYYHQSM"
+  };
+
+
+const firebaseApp = firebase.intializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
